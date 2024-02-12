cbb72d7ab1f393b02a5f4d9ef51a9a897df723a3
Damindra Gunatillake <damindra@damindra.co.uk>
2024-02-12 19:54:47 +0000

    Added definition of Fibonacci sequence and what we are attempting to find

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+
+/*
+
+The Fibonacci sequence is defined by the recurrence relation:
+F(n)= F (n - 1) + F (n - 2), where F(1) = 1 and F(2) = 1.
+Hence the first 12 terms will be:
+F(1) = 1
+F(2) = 1
+F(3) = 2
+F(4) = 3
+F(5) = 5
+F(6) = 8
+F(7) = 13
+F(8) = 21
+F(9) = 34
+F(10) = 55
+F(11) = 89
+F(12) = 144
+The term, F(12), is the first term to contain three digits.
+Write a program to find the index of the first term in the Fibonacci sequence
+to contain 10 digits?
+
+*/ 
+
 using namespace std;
 
 int main() {
